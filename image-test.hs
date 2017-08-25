c2ef9d1ced6db198f4b9d59acac5a24bd7fd394e
pdubs <all.cows.like.to.moo@gmail.com>
2017-08-24 17:57:50 -0700

    more arrangement

--- image-test.hs
+++ image-test.hs
@@ -18,29 +18,22 @@ main = do
   -- Here we should parse arguements, then scene file
   savePngImage path generateImg
 
-imgWidth = 800
-imgHeight = 600
+imgWidth = 200
+imgHeight = 150
 
 generateImg :: DynamicImage
 generateImg = ImageRGB8 (generateImage colorPixel imgWidth imgHeight)
 
---wiggle n = round (128.0 * sin (0.01 * fromIntegral n) + 128.0)
-
 -- Scene (would be nice to use actual types for different objects)
 
-background = [0.08,0.1,0.15]
+background = [0.06,0.08,0.12]
 
-cameraPos = [0,0,-3]
+cameraPos = [0,0,-2]
 
 spherePos = [0,0,0]
-sphereRadius = 1.0
-sphereWiggleFreq = 7.0
-sphereWiggleAmp = 0.25
 sphereColor = [0.9,0.95,1]
 
-planeHeight = -0.8
-
-reflectivity = 0.6
+planeHeight = -0.6
 
 lightPos = [2.2, 10.0, -10.6]
 lightAmp = 1.0
@@ -48,11 +41,11 @@ lightAmp = 1.0
 -- Intersection and lighting parameters
 escapeDistance = 100.0
 escapeFlag = [-999,-999,-999]
-intersectThreshold = 0.00005
+intersectThreshold = 0.005
 eps = 0.00001
-stepSize = 0.998
+stepSize = 0.2--0.998
 maxSteps = 500
-maxBounces = 4
+--maxBounces = 4
 
 fov = 1.0
 
@@ -61,8 +54,7 @@ colorPixel x y = PixelRGB8 (toPix (col!!0))
                            (toPix (col!!1))
                            (toPix (col!!2))
                            where col = colorThroughPixel x y
-
-toPix x = round (min (255.0 * x) 255.0)
+                                 toPix x = round (min (255.0 * x) 255.0)
 
 colorThroughPixel :: Int -> Int -> [Double]
 colorThroughPixel x y = 
@@ -109,8 +101,9 @@ intersect origin direction = intersect' origin (normalize direction) 0
       where d = sceneSDF origin
 
 sceneSDF :: [Double] -> Double
-sceneSDF v = minimum $ [ wildSDF v [x,z,z] (x*z) (0.08*x) 0.3 | 
-  x <- [-2..2], z <- [0..5] ] ++ [planeSDF v]
+sceneSDF v = minimum $ [ wildSDF v [x,z*0.5,z] (3*am*(z+1)) (0.15*am) 0.3 | 
+  x <- [-wid..wid], z <- [0..5], let am = wid - (abs x) ] ++ [planeSDF v]
+  where wid = 2
 
 planeSDF :: [Double] -> Double
 planeSDF v = dot v [0,1,0] - planeHeight
